f4527439b940c2df500139af5516344d8a818cb3
vijaygajera <30596006+vijaygajera@users.noreply.github.com>
2018-02-17 12:29:40 +0530

    Update hooks.py

--- vijaymamra/hooks.py
+++ vijaymamra/hooks.py
@@ -75,8 +75,7 @@ fixtures = ["Custom Field", "Property Setter", "Print Format", "Report", "Custom
 # has_permission = {
 # 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
-doctype_js={
-	"Sales Order":"vijaymamra/custom_script/sales_order/sales_order.js",
+
 # Document Events
 # ---------------
 # Hook on document methods and events
