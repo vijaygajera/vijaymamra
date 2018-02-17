dea09d3679c0a139a96db0e423a56e97af4ba027
vijaygajera <30596006+vijaygajera@users.noreply.github.com>
2018-02-17 15:04:01 +0530

    Update sales_order.py

--- vijaymamra/vijaymamra/custom_script/sales_order/sales_order.py
+++ vijaymamra/vijaymamra/custom_script/sales_order/sales_order.py
@@ -4,8 +4,8 @@ from frappe.model.document import Document
 import decimal
 
 @frappe.whitelist()
-def calculate_row_quantity(bag_qty, rate, uom):
-	bag = float(bag_qty)
+def calculate_row_quantity(bag, rate, uom):
+	b = float(bag)
 	conv_factor = float(uom)
-	row_qty = bag * conv_factor
+	row_qty = b * conv_factor
 	return round(row_qty)
