4c1dbdf631e1b6af2f19275df67df26a448d72cb
vijaygajera <30596006+vijaygajera@users.noreply.github.com>
2018-03-05 17:20:36 +0530

    Update delivery_note.js

--- vijaymamra/public/js/delivery_note.js
+++ vijaymamra/public/js/delivery_note.js
@@ -1,4 +1,5 @@
 cur_frm.add_fetch('item_code', 'weight_per_bag', 'weight_per_bag')
+consol_log("===========")
 frappe.ui.form.on("Delivery Note Item", "bag", function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
 	result_value = child.bag * child.weight_per_bag;
