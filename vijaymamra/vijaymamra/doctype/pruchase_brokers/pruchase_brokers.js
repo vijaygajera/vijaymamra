3363e7bdd9e9c641e3276618abf8e547e2d74bbf
Frappe <vijaygajera@gmail.com>
2018-03-19 00:59:04 -0700

    ninteenth march commit

---
+++ vijaymamra/vijaymamra/doctype/pruchase_brokers/pruchase_brokers.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, vijaymamra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pruchase Brokers', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ vijaymamra/vijaymamra/doctype/purchase_brokers/purchase_brokers.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, vijaymamra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Purchase Brokers', {
+	refresh: function(frm) {
+
+	}
+});
