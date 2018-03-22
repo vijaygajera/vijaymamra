8121d0671bc60f135cc13206aca7cac776e6c325
Frappe <vijaygajera@gmail.com>
2018-03-22 03:39:45 -0700

    22march

---
+++ vijaymamra/vijaymamra/doctype/truck_inword/truck_inword.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2018, vijaymamra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Truck Inword', {
+	refresh: function(frm) {
+
+	}
+});
+
+
+//frappe.ui.form.on("Truck Inword", {
+//	setup: function(frm) {
+//		frm.custom_make_buttons = {
+//			'close': 'close_sales_order',
+
+//close_sales_order: function(){
+//		this.frm.cscript.update_status("Close", "Closed")
+		
+
+				//if (this.frm.has_perm("submit")) {
+					// close
+				//	if(flt(doc.per_delivered, 2) < 100 || flt(doc.per_billed) < 100) {
+				//		this.frm.add_custom_button(__('Close'),
+				//			function() { me.close_sales_order() }, __("Status"))
